d96e3485fc352bbb3b831700f066bdb7dea3cc70
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2014-07-01 12:11:53 +0200

    [TASK] Added scaffold for Coordinate Validator

---
+++ Classes/Subugoe/GermaniaSacra/Validation/Validator/CoordinateValidator.php
@@ -0,0 +1,46 @@
+<?php
+namespace Subugoe\GermaniaSacra\Validation\Validator;
+use TYPO3\Flow\Annotations as Flow;
+
+/* * *************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2014 Ingo Pfennigstorf <pfennigstorf@sub-goettingen.de>
+ *      Goettingen State Library
+ *  
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ * ************************************************************* */
+
+/**
+ * Validator for coordinates
+ */
+class CoordinateValidator extends \TYPO3\Flow\Validation\Validator\AbstractValidator {
+	/**
+	 * Check if $value is valid. If it is not valid, needs to add an error
+	 * to Result.
+	 *
+	 * @param mixed $value
+	 * @return void
+	 * @throws \TYPO3\Flow\Validation\Exception\InvalidValidationOptionsException if invalid validation options have been specified in the constructor
+	 */
+	protected function isValid($value) {
+		// TODO: Implement isValid() method.
+	}
+
+} 
