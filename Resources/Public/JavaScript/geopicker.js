4fa282d0ef90a23ffecf28632e7dd0ba0007f848
tschaef <tschaef1@gwdg.de>
2014-07-02 19:00:46 +0200

    Replaced Google Maps Satellite with ESRI World Imagery

--- Resources/Public/JavaScript/geopicker.js
+++ Resources/Public/JavaScript/geopicker.js
@@ -21,39 +21,43 @@ $( function() {
 			event.stopPropagation()
 		});
 
-		// Define map layers (note that Google requires plugin)
+		// Define map layers
 		var layers = []
-		layers['mapbox'] = L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+		layers.mapbox = L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
 			maxZoom: 18,
 			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
 			id: 'examples.map-i86knfo3',
 		})
-		layers['osm'] = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-		    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+		layers.osm = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 		})
-		layers['googleStreet'] = new L.Google('ROADMAP')
-		layers['googleHybrid'] = new L.Google('HYBRID')
+		layers.esri = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+			attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+		})
+		layers.thunderforest = L.tileLayer('http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png', {
+			attribution: '&copy; <a href="http://www.opencyclemap.org">OpenCycleMap</a>, &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+		})
 
 		// Define layer controls
-		baseMaps = {
-			"Mapbox": layers['mapbox'],
-			"OpenStreetMap": layers['osm'],
-			"Google (Straßen)": layers['googleStreet'],
-			"Google (Hybrid)": layers['googleHybrid'],
+		var baseMaps = {
+			"Mapbox": layers.mapbox,
+			"OpenStreetMap": layers.osm,
+			"ESRI World Imagery": layers.esri,
+			"Thunderforest Landscape": layers.thunderforest,
 		}
-		overlayMaps = {}
+		var overlayMaps = {}
 
 		// Draw the map and add controls, center over and zoom to Germany
 		var map = L.map('map', {
 			center: [51.163, 10.448],
 			zoom: 6,
-			layers: layers
+			layers: layers,
 		})
 		L.control.layers(baseMaps, overlayMaps).addTo(map)
 		L.control.scale().addTo(map)
 		map.invalidateSize()
 
-		// WORKAROUND: With Google plugin, leaflet does not show any layer on load, so let's force it
+		// Load first map in list
 		$('#map .leaflet-control-layers-base input:eq(0)').click()
 
 		// Insert coordinates at pointer into input fields
@@ -61,7 +65,7 @@ $( function() {
 			// TODO: input[type=number] is localized, so for DE the decimal separator becomes a comma, while JS still uses a point
 			var lat = e.latlng.lat.toFixed(6)
 			var lng = e.latlng.lng.toFixed(6)
-			// Insert values into input fields, cut to 6 decimals (correspongs to a precision of about 10 cm)
+			// Insert values into input fields, cut to 6 decimals (corresponds to a precision of about 10 cm)
 			$container.find(':input[name$="breite[]"]').val( lat )
 			$container.find(':input[name$="laenge[]"]').val( lng )
 			//L.popup()
