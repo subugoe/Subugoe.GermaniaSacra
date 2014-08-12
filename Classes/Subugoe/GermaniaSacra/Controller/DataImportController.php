04930c04956e8b307b498922500cbbbd89de07d2
Ali Sajedi <sajedi@sub.uni-goettingen.de>
2014-08-12 09:55:58 +0200

    [FEATURE] ADWD-844 DataImportController adapted to account for missing urls

--- Classes/Subugoe/GermaniaSacra/Controller/DataImportController.php
+++ Classes/Subugoe/GermaniaSacra/Controller/DataImportController.php
@@ -632,7 +632,6 @@ class DataImportController extends ActionController {
 				$this->persistenceManager->persistAll();
 			}
 		}
-
 	}
 
 	/**
@@ -836,23 +835,6 @@ class DataImportController extends ActionController {
 			$this->persistenceManager->persistAll();
 			$wikiurltypUUID = $urltypObject->getUUID();
 		}
-
-		// Added to prevent wrong search result
-		$defaultUrlTypeName = "keine Angabe";
-		$defaultUrltypObject = new Urltyp();
-		$defaultUrltypObject->setName($defaultUrlTypeName);
-		$this->urltypRepository->add($defaultUrltypObject);
-		$this->persistenceManager->persistAll();
-
-
-		$defaultUrl = "keine Angabe";
-		$defaultUrlObject = new Url();
-		$defaultUrlObject->setUrl($defaultUrl);
-		$defaultUrlObject->setUrltyp($defaultUrltypObject);
-		$this->urlRepository->add($defaultUrlObject);
-		$this->persistenceManager->persistAll();
-
-
 		$sql = 'SELECT * FROM Kloster ORDER BY Klosternummer ASC';
 		$klosters = $sqlConnection->fetchAll($sql);
 		if (isset($klosters) and is_array($klosters)) {
@@ -1064,23 +1046,36 @@ class DataImportController extends ActionController {
 					}
 				}
 			}
+		}
+	}
 
-			// Added to prevent wrong search result
-			$checkForKlosterHasUrlObject = $this->klosterHasUrlRepository->findByIdentifier($klosterUUID);
-			if (!is_object($checkForKlosterHasUrlObject)) {
+	/**
+	* Adds a default URL for to prevent wrong search result
+	* @return void
+	*/
+	public function addDefaultUrlAction() {
+		$defaultUrlTypeName = "keine Angabe";
+		$defaultUrltypObject = new Urltyp();
+		$defaultUrltypObject->setName($defaultUrlTypeName);
+		$this->urltypRepository->add($defaultUrltypObject);
+		$this->persistenceManager->persistAll();
+		$defaultUrl = "keine Angabe";
+		$defaultUrlObject = new Url();
+		$defaultUrlObject->setUrl($defaultUrl);
+		$defaultUrlObject->setUrltyp($defaultUrltypObject);
+		$this->urlRepository->add($defaultUrlObject);
+		$this->persistenceManager->persistAll();
 
-				echo $klosterUUID . '\n';
-
+		$klosters = $this->klosterRepository->findAll();
+		foreach ($klosters as $kloster) {
+			$urls = $kloster->getKlosterHasUrls();
+			if (count($urls) == 0) {
 				$klosterHasUrlObject = new KlosterHasUrl();
-				$klosterHasUrlObject->setKloster($checkForKlosterHasUrlObject);
+				$klosterHasUrlObject->setKloster($kloster);
 				$klosterHasUrlObject->setUrl($defaultUrlObject);
-				$this->urlRepository->add($klosterHasUrlObject);
+				$this->klosterHasUrlRepository->add($klosterHasUrlObject);
 				$this->persistenceManager->persistAll();
 			}
-			else {
-				echo 'kein Object' . $klosterUUID . '\n';
-			}
-
 		}
 	}
 
