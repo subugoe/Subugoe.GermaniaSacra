817d0ca3eed5d212ee2c975ece297472f6b83e94
Ali Sajedi <sajedi@sub.uni-goettingen.de>
2014-09-03 17:33:06 +0200

    [TASK] land entries update

--- Classes/Subugoe/GermaniaSacra/Controller/LandController.php
+++ Classes/Subugoe/GermaniaSacra/Controller/LandController.php
@@ -9,27 +9,27 @@ use TYPO3\Flow\Mvc\Controller\RestController;
 class LandController extends ActionController {
 
 	/**
-	 * @Flow\Inject
-	 * @var \Subugoe\GermaniaSacra\Domain\Repository\LandRepository
-	 */
+	* @Flow\Inject
+	* @var \Subugoe\GermaniaSacra\Domain\Repository\LandRepository
+	*/
 	protected $landRepository;
 
 	/**
-	 * @var array
-	 */
+	* @var array
+	*/
 	protected $supportedMediaTypes = array('text/html', 'application/json');
 
 	/**
-	 * @var array
-	 */
+	* @var array
+	*/
 	protected $viewFormatToObjectNameMap = array(
 			'json' => 'TYPO3\\Flow\\Mvc\\View\\JsonView',
 			'html' => 'TYPO3\\Fluid\\View\\TemplateView'
 	);
 
 	/**
-	 * @return void
-	 */
+	* @return void
+	*/
 	public function listAction() {
 		if ($this->request->getFormat() === 'json') {
 			$this->view->setVariablesToRender(array('land'));
@@ -38,38 +38,55 @@ class LandController extends ActionController {
 	}
 
 	/**
-	 * @param \Subugoe\GermaniaSacra\Domain\Model\Land $land
-	 * @return void
-	 */
+	* @param \Subugoe\GermaniaSacra\Domain\Model\Land $land
+	* @return void
+	*/
 	public function showAction(Land $land) {
 		$this->view->setVariablesToRender(array('land'));
 		$this->view->assign('land', $land);
 	}
 
 	/**
-	 * @param \Subugoe\GermaniaSacra\Domain\Model\Land $newBand
-	 * @return void
-	 */
+	* @param \Subugoe\GermaniaSacra\Domain\Model\Land $newBand
+	* @return void
+	*/
 	public function createAction(Land $newBand) {
 		$this->landRepository->add($newBand);
 		$this->response->setStatus(201);
 	}
 
 	/**
-	 * @param \Subugoe\GermaniaSacra\Domain\Model\Land $land
-	 * @return void
-	 */
+	* @param \Subugoe\GermaniaSacra\Domain\Model\Land $land
+	* @return void
+	*/
 	public function updateAction(Land $land) {
 		$this->landRepository->update($land);
 	}
 
 	/**
-	 * @param \Subugoe\GermaniaSacra\Domain\Model\Land $land
-	 * @return void
-	 */
+	* @param \Subugoe\GermaniaSacra\Domain\Model\Land $land
+	* @return void
+	*/
 	public function deleteAction(Land $land) {
 		$this->landRepository->remove($land);
 	}
-}
 
+	/**
+	* Updates the list of Land
+	* @return void
+	*/
+	public function listupdateAction() {
+		$lands = $this->request->getArguments();
+		foreach ($lands as $uuid => $land) {
+			$landObj = $this->landRepository->findByIdentifier($uuid);
+			$landObj->setLand($land['land']);
+			$landObj->setIst_in_deutschland($land['ist_in_deutschland']);
+			$this->landRepository->update($landObj);
+		}
+
+		$this->persistenceManager->persistAll();
+
+		$this->throwStatus(200, NULL, Null);
+	}
+}
 ?>
