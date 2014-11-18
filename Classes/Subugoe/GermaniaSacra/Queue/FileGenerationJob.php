00b9a016ff4b3ef10d362a9409466341710c32ff
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2014-11-18 21:07:18 +0100

    [BUGFIX] Fixes in FileGenerator

--- Classes/Subugoe/GermaniaSacra/Queue/FileGenerationJob.php
+++ Classes/Subugoe/GermaniaSacra/Queue/FileGenerationJob.php
@@ -26,7 +26,6 @@ namespace Subugoe\GermaniaSacra\Queue;
  *  This copyright notice MUST APPEAR in all copies of the script!
  * ************************************************************* */
 
-use Subugoe\GermaniaSacra\Utility\JsonGeneratorUtility;
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Jobqueue\Common\Job\JobInterface;
 use TYPO3\Jobqueue\Common\Queue\Message;
@@ -45,9 +44,15 @@ class FileGenerationJob implements JobInterface {
 
 	/**
 	 * @var \Subugoe\GermaniaSacra\Service\JsonGeneratorService
-	 * @FLOW\Inject
+	 * @Flow\Inject
 	 */
 	protected $jsonGeneratorService;
+
+	/**
+	 * @var \TYPO3\Flow\Log\Logger
+	 * @Flow\Inject
+	 */
+	protected $logger;
 
 	public function __construct($entityName) {
 		$this->entityName = $entityName;
@@ -63,7 +68,15 @@ class FileGenerationJob implements JobInterface {
 	 * @return boolean TRUE if the job was executed successfully and the message should be finished
 	 */
 	public function execute(QueueInterface $queue, Message $message) {
-		return $this->jsonGeneratorService->generateJsonFile($this->entityName);
+
+		$jobExecuted = FALSE;
+
+		try {
+			$jobExecuted = $this->jsonGeneratorService->generateJsonFile($this->entityName);
+		} catch (\Exception $e) {
+			$this->logger->logException($e);
+		}
+		return $jobExecuted;
 	}
 
 	/**
@@ -81,6 +94,6 @@ class FileGenerationJob implements JobInterface {
 	 * @return string A label for the job
 	 */
 	public function getLabel() {
-		return 'KlosterFile';
+		return 'jsonjob';
 	}
 }
